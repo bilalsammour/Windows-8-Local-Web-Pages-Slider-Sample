2f314ff8730cc5b43a2903b6ef22717c51916f3c
bilalsammour <bilalsammour@gmail.com>
2014-10-18 18:15:20 +0300

    source item

---
+++ LocalWebPagesSlider/LocalWebPagesSlider.Shared/DataModel/SampleDataSource.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+
+namespace LocalWebPagesSlider.DataModel
+{
+    [DataContract]
+    public class SampleDataSource
+    {
+        public SampleDataSource()
+        {
+            WebPages = new ObservableCollection<WebPageItem>();
+        }
+
+        [DataMember(Name = "WebPages")]
+        public ObservableCollection<WebPageItem> WebPages { get; private set; }
+    }
+}
